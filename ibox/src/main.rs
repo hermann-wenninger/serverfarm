249c2d15e40d4841254f862724fb8fad7756bddd
wennhmann <info@wennhmann.de>
2025-08-25 14:40:54 +0200

    ref box with **

--- ibox/src/main.rs
+++ ibox/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
    let mut x = Box::new(5);
     *x += 1;
-    println!("Wert in Box: {}", x);
+   let y = &x;
+    
+    println!("Wert in Box: {}", &x);
+    println!("Referenz auf Box: {}", **y);
 }
