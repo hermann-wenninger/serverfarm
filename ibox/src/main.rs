4928017b8531d358c467ffa68cecd85e5027676a
wennhmann <info@wennhmann.de>
2025-08-25 19:38:46 +0200

    deref display drop

--- ibox/src/main.rs
+++ ibox/src/main.rs
@@ -1,3 +1,28 @@
+use std::ops::Deref;
+
+struct MioBox<T>(T);
+
+impl <T> MioBox<T> {
+    fn new(x: T) -> MioBox<T> {
+        MioBox(x)
+    }
+}
+impl<T> Deref for MioBox<T>{
+    type Target =T;
+    fn deref(&self)->&Self::Target{
+        &self.0
+    }
+}
+impl<T: std::fmt::Display> MioBox<T> {
+    fn print(&self) {
+        println!("MioBox enthält: {}", self.0);
+    }
+}
+impl<T> Drop for MioBox<T> where T: std::fmt::Debug {
+    fn drop(&mut self){
+        println!("MioBox wird gelöscht {:?}",&self.0);
+    }
+}
 fn main() {
    let mut x = Box::new(5);
     *x += 1;
@@ -5,4 +30,9 @@ fn main() {
     
     println!("Wert in Box: {}", &x);
     println!("Referenz auf Box: {}", **y);
+
+    let mio = MioBox::new(23);
+    println!("Wert in MioBox: {}", mio.0);
+    println!("Wert in MioBox via Deref: {}", *mio+1);
+    drop(mio);
 }
