6c13825eb63c46d8b45751f1e4c3225aaf70c4ea
wennhmann <info@wennhmann.de>
2025-08-26 20:40:46 +0200

    file not found

--- error/src/main.rs
+++ error/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{self, Read};
 
 fn main() -> io::Result<()> {
-    let mut file = File::open("file.txt")?;
+    let mut file = File::open("./file.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     println!("Dateiinhalt: {}", contents);
