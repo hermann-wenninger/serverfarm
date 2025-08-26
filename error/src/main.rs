f24d7429dfa1027601a78a0e047b2ce7a1260676
wennhmann <info@wennhmann.de>
2025-08-26 17:06:10 +0200

    std prelude

--- error/src/main.rs
+++ error/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, Read};
+
+fn main() -> io::Result<()> {
+    let mut file = File::open("file.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    println!("Dateiinhalt: {}", contents);
+    Ok(())
 }
