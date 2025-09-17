e7bb3251e065d784531908e0595977d9844ab35c
wennhmann <info@wennhmann.de>
2025-09-17 17:46:20 +0200

    sono domande in spacio

--- marker/src/main.rs
+++ marker/src/main.rs
@@ -28,11 +28,11 @@ fn main() {
     let mut net_res = NetworkResource{address: String::from("127.0.0.1:8080"), closed: false};
     let mut db_res = DatabaseResource{connection_string: String::from("Server=localhost;Database=mydb;"), closed: false};
     
-    let mut res_closable:Vec< dyn Closable> = Vec::new();
-    res_closable.push(file_res);
-    res_closable.push(net_res);
-    res_closable.push(db_res);
-    println!("{} resources to close", res_closable);
+    let mut res_closable:Vec< Box<dyn Closable>> = Vec::new();
+   res_closable.push(Box::new(file_res));
+res_closable.push(Box::new(net_res));
+res_closable.push(Box::new(db_res));
+    println!("{:?} resources to close", res_closable);
     
     close_resource(&mut file_res);
     close_resource(&mut net_res);
