ede7720215b726bbd3b1b1345f02cc75bd907060
wennhmann <info@wennhmann.de>
2025-09-18 16:50:10 +0200

    training3

--- training/src/main.rs
+++ training/src/main.rs
@@ -1,6 +1,6 @@
 
 fn main() {
 
-println!("r# raw string \n no new line");
-println!(r#"r# raw string with "quotes" \n no new line"#);
+
+println!(r#"r# raw string with "quotes" \n  \r no new line"#);
 }
