7e2409395b3f51c25da9577e53f8e2638d44a368
wennhmann <info@wennhmann.de>
2025-09-18 16:48:16 +0200

    training3

--- training/src/main.rs
+++ training/src/main.rs
@@ -1,10 +1,6 @@
-fn time_two(number:i32)-> i32{
-    number + number
-}
-
 
 fn main() {
-    let arg = 5;
-    let answer = time_two(arg);
-    println!("The answer is: {}", answer);
+
+println!("r# raw string \n no new line");
+println!(r#"r# raw string with "quotes" \n no new line"#);
 }
