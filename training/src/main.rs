1f06da41173a7209a2a3db490ddbd47ea2a2ea50
wennhmann <info@wennhmann.de>
2025-09-19 15:56:30 +0200

    train6

--- training/src/main.rs
+++ training/src/main.rs
@@ -12,4 +12,8 @@ x.push(name2);
 x.push(name3);
 x.push(name4);
 println!("{:#?}",&x as &[&str] );
+println!("{:?}",x );
+let y= vec![1,2,3,4,5,6,7,8,9,10];
+println!("{:#?}",&y as &[i32] );
+println!("{:?}",&y[1..3] );
 }
